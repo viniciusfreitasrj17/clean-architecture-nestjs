9c77610b6a5da1de78eaf78c41296a719fb10e76
Marcos Vinicius <viniciusfreitasrj17@gmail.com>
2023-01-11 23:35:10 -0300

    add resource bank-accounts

---
+++ src/bank-accounts/bank-accounts.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get, Post, Body, Param, HttpCode } from '@nestjs/common';
+import { BankAccountsService } from './bank-accounts.service';
+import { CreateBankAccountDto } from './dto/create-bank-account.dto';
+import { TransferBankAccountDto } from './dto/tranfer-bank-account.dto';
+
+@Controller('bank-accounts')
+export class BankAccountsController {
+  constructor(private readonly bankAccountsService: BankAccountsService) {}
+
+  @Post()
+  create(@Body() createBankAccountDto: CreateBankAccountDto) {
+    return this.bankAccountsService.create(createBankAccountDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.bankAccountsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.bankAccountsService.findOne(id);
+  }
+
+  @HttpCode(204)
+  @Post('transfer')
+  transfer(@Body() transferBankAccountDto: TransferBankAccountDto) {
+    const { from, to, amount } = transferBankAccountDto;
+    return this.bankAccountsService.transfer(from, to, amount);
+  }
+}
---
+++ src/bank-accounts/bank-accounts.service.ts
@@ -0,0 +1,54 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { getDataSourceToken, InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { CreateBankAccountDto } from './dto/create-bank-account.dto';
+import { BankAccount } from './entities/bank-account.entity';
+
+@Injectable()
+export class BankAccountsService {
+  constructor(
+    @InjectRepository(BankAccount)
+    private repo: Repository<BankAccount>,
+    @Inject(getDataSourceToken())
+    private dataSource: DataSource,
+  ) {}
+
+  async create(createBankAccountDto: CreateBankAccountDto) {
+    const { account_number } = createBankAccountDto;
+    const bankAccount = this.repo.create({
+      account_number,
+      balance: 0,
+    });
+
+    await this.repo.insert(bankAccount);
+  }
+
+  findAll() {
+    return this.repo.find();
+  }
+
+  findOne(id: string) {
+    return this.repo.findOneBy({ id });
+  }
+
+  async transfer(from: string, to: string, amount: number) {
+    // transation mode
+    const queryRunner = this.dataSource.createQueryRunner();
+    try {
+      await queryRunner.startTransaction();
+      const fromAccount = await this.repo.findOneBy({ account_number: from });
+      const toAccount = await this.repo.findOneBy({ account_number: to });
+
+      fromAccount.balance -= amount;
+      toAccount.balance += amount;
+
+      this.repo.save(fromAccount);
+      this.repo.save(toAccount);
+      await queryRunner.commitTransaction();
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+      console.log(e);
+      throw e;
+    }
+  }
+}
